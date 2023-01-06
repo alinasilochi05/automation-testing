8ea4d64ec3d76beac2055c65f73229e0672f45f9
Alina S <alinamihaela525@gmail.com>
2023-01-06 17:36:36 +0200

    - select add to wishlist element -create a method for click on add to wishlist -create test for add to wishlist functionality

---
+++ src/pages/NavBarPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class NavBarPage {
+    WebDriver chromeDriver;
+
+    public NavBarPage(WebDriver chromeDriver){
+        this.chromeDriver = chromeDriver;
+    }
+
+}
