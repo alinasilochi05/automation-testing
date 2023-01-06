cb3178abe8fb2d2c4c41b167ff305ca45fa72585
Alina S <alinamihaela525@gmail.com>
2023-01-06 22:48:05 +0200

    -create assert for opening edit details page from navBar

--- src/tests/TestNavBar.java
+++ src/tests/TestNavBar.java
@@ -1,18 +1,24 @@
 package tests;
 
 import constants.Urls;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.EditAccountDetailsPage;
 import pages.NavBarPage;
 
 public class TestNavBar extends TestBase{
     NavBarPage navBarPage;
+    EditAccountDetailsPage editAccountDetailsPage;
     public TestNavBar(){
         super(Urls.signin);
         this.navBarPage = new NavBarPage(chromeDriver);
+        this.editAccountDetailsPage = new EditAccountDetailsPage(chromeDriver);
     }
     @Test
     public void openCustomerMenuCategory(){
         this.navBarPage.hoverElement(1);
         this.navBarPage.clickOnSubmenuCustomerNav(3);
+        Assert.assertEquals(this.editAccountDetailsPage.getEditAccountDetailsTitlePage(), String.format("%S", "My Account Information"));
+
     }
 }
