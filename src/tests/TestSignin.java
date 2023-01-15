68be32b0326773b5b2765fad54efe4076b4d853b
Alina S <alinamihaela525@gmail.com>
2023-01-15 23:26:55 +0200

    fixed: create a new before suite fore signin tests

--- src/tests/TestSignin.java
+++ src/tests/TestSignin.java
@@ -2,9 +2,10 @@ package tests;
 
 import constants.Urls;
 import handlers.SigninHandler;
-import org.junit.Test;
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 import pages.SigninPage;
 
 public class TestSignin extends TestBase {
@@ -13,57 +14,67 @@ public class TestSignin extends TestBase {
     SigninHandler signinHandler;
 
     public TestSignin() {
-        super(Urls.signin);
+        super();
         this.signinPage = new SigninPage(this.chromeDriver);
         this.signinHandler = new SigninHandler(this.chromeDriver);
     }
-//    @BeforeTest
-//    public void setUpBeforeTest(){
-//        this.chromeDriver.get(Urls.signin);
-//    }
 
-    @Test()
+    @BeforeSuite()
+    @Override()
+    public void setUpBeforeSuite() {}
+
+    @BeforeTest
+    public void setUpBeforeTest(){
+        this.chromeDriver.get(Urls.signin);
+    }
+
+    @Test
     public void loginValidData() {
         this.signinHandler.Signin();
     }
 
     @Test
-    public void loginInvalidPassword(){
+    public void loginInvalidPassword() {
         this.signinPage.setLoginName("gloria");
         this.signinPage.setLoginPassword("pas");
         this.signinPage.clickLogin();
         Assert.assertEquals(this.signinPage.getWarningMessage(), this.signinPage.warningMessage);
     }
+
     @Test
-    public void loginInvalidUsername(){
+    public void loginInvalidUsername() {
         this.signinPage.setLoginName("glo");
         this.signinPage.setLoginPassword("pass");
         this.signinPage.clickLogin();
         Assert.assertEquals(this.signinPage.getWarningMessage(), this.signinPage.warningMessage);
     }
+
     @Test
-    public void loginEmptyPassword(){
+    public void loginEmptyPassword() {
         this.signinPage.setLoginName("gloria");
         this.signinPage.setLoginPassword(" ");
         this.signinPage.clickLogin();
         Assert.assertEquals(this.signinPage.getWarningMessage(), this.signinPage.warningMessage);
     }
+
     @Test
-    public void loginEmptyUsername(){
+    public void loginEmptyUsername() {
         this.signinPage.setLoginName(" ");
         this.signinPage.setLoginPassword("pass");
         this.signinPage.clickLogin();
         Assert.assertEquals(this.signinPage.getWarningMessage(), this.signinPage.warningMessage);
     }
+
     @Test
-    public void loginCapitalUsername(){
+    public void loginCapitalUsername() {
         this.signinPage.setLoginName("GLORIA");
         this.signinPage.setLoginPassword("pass");
         this.signinPage.clickLogin();
         Assert.assertEquals(this.signinPage.getWarningMessage(), this.signinPage.warningMessage);
     }
+
     @Test
-    public void loginCapitalPassword(){
+    public void loginCapitalPassword() {
         this.signinPage.setLoginName("gloria");
         this.signinPage.setLoginPassword("PASS");
         this.signinPage.clickLogin();
